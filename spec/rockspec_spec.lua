d445df9cbce7f502c4fb30a41c18592d0ac96c4c
Marc Jakobi <marc@jakobi.dev>
2026-09-23 02:21:48 +0200

    feat: support using scm rockspecs as templates (#991)

--- spec/rockspec_spec.lua
+++ spec/rockspec_spec.lua
@@ -35,6 +35,39 @@ build = {
 }
 ]]
 
+local scm_template = [[
+local git_ref = '$git_ref'
+local modrev = '$modrev'
+local is_release = '$is_release' == 'true'
+
+local repo_url = 'https://github.com/nvim-neorocks/luarocks-tag-release'
+
+rockspec_format = '3.0'
+package = 'test_package'
+
+dependencies = {
+  'lua >= 5.1',
+}
+
+build = {
+  type = 'builtin',
+  copy_directories = { 'plugin' },
+}
+
+if is_release then
+  version = modrev .. '-' .. '$specrev'
+  source = {
+    url = repo_url .. '/archive/' .. git_ref .. '.zip',
+    dir = '$repo_name-' .. git_ref,
+  }
+else
+  version = 'scm-1'
+  source = {
+    url = repo_url:gsub('https', 'git'),
+  }
+end
+]]
+
 ---@param rockspec_str string The rockspec content
 local function load_rockspec(rockspec_str)
   local rockspec_module = assert(loadstring(rockspec_str), 'Could not load generated rockspec')
@@ -81,5 +114,26 @@ describe('Rockspec', function()
     meta.dependencies = { 'lua >= 5.4' }
     load_rockspec(Rockspec.generate('test_package', '1.0.0', '1', test_template, meta))
     assert.same(dependencies, { 'lua >= 5.4' })
+  end)
+  it('Generates a release rockspec from an scm template', function()
+    load_rockspec(Rockspec.generate('test_package', '1.0.0', '1', scm_template, meta))
+    assert.same(version, '1.0.0-1')
+    assert.same(source.url, 'https://github.com/nvim-neorocks/luarocks-tag-release/archive/1.0.0.zip')
+    assert.same(source.dir, 'luarocks-tag-release-1.0.0')
+  end)
+  it('Loads the same scm template standalone as an scm rockspec', function()
+    load_rockspec(scm_template)
+    assert.same(version, 'scm-1')
+    assert.same(source.url, 'git://github.com/nvim-neorocks/luarocks-tag-release')
+  end)
+  it('Generates an scm rockspec from an scm template when version is scm', function()
+    load_rockspec(Rockspec.generate('test_package', 'scm', '1', scm_template, meta))
+    assert.same(version, 'scm-1')
+    assert.same(source.url, 'git://github.com/nvim-neorocks/luarocks-tag-release')
+  end)
+  it('Generates an scm rockspec from an scm template when version is dev', function()
+    load_rockspec(Rockspec.generate('test_package', 'dev', '1', scm_template, meta))
+    assert.same(version, 'scm-1')
+    assert.same(source.url, 'git://github.com/nvim-neorocks/luarocks-tag-release')
   end)
 end)
